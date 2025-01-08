5fe3075574229241bb8411e86bcfe7a3cf47bdbb
furkan.emmezoglu <furkan.emmezoglu@trendyol.com>
2025-01-08 23:01:56 +0300

    Add web config

--- src/main/kotlin/com/fuki/hatimmatik/infrastructure/web/user/SectionController.kt
+++ src/main/kotlin/com/fuki/hatimmatik/infrastructure/web/user/SectionController.kt
@@ -2,12 +2,14 @@ package com.fuki.hatimmatik.infrastructure.web.user
 
 import com.fuki.hatimmatik.application.user.service.SectionService
 import com.fuki.hatimmatik.domain.user.core.Section
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@CrossOrigin(origins = ["*"])
 class SectionController(
     val service: SectionService,
 ) {
--- src/main/kotlin/com/fuki/hatimmatik/infrastructure/web/user/UserController.kt
+++ src/main/kotlin/com/fuki/hatimmatik/infrastructure/web/user/UserController.kt
@@ -2,11 +2,13 @@ package com.fuki.hatimmatik.infrastructure.web.user
 
 import com.fuki.hatimmatik.application.user.service.FindUserService
 import com.fuki.hatimmatik.domain.user.core.User
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@CrossOrigin(origins = ["*"])
 class UserController(
     val service: FindUserService,
 ) {
